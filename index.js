5b8e5dd82ff21bc0deb6f8b5f2a7a4712f4f930f
Danil gdj <danilguidjou7@gmail.com>
2024-08-24 15:12:13 +0100

    last commit

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const http = require('node:http');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-// Create a local server to receive data from
-const server = http.createServer();
-
-// Listen to the request event
-server.on('request', (request, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!',
-  }));
-});
-
-server.listen(8009);
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  if(q.pathname == "/"){
+    var filename = "./index.html"
+  }else{
+    var filename = "." + q.pathname + ".html";
+  }
+  
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      fs.readFile("404.html" , function(err , dat){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(dat);
+        return res.end()
+      })
+    } else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    }
+    
+  });
+}).listen(8080);
